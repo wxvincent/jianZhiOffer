123860b0788cdededcb3bd942b5dea0c1bfce3df
itwx <942774363@qq.com>
2019-06-08 09:19:32 +0800

    6/8上午提交

--- src/cn/itwx7/Offer63.java
+++ src/cn/itwx7/Offer63.java
@@ -2,29 +2,25 @@ package cn.itwx7;
 
 /**
  * 股票的最大利润
- * @author wang
  *
+ * @author wang
  */
 public class Offer63 {
 
-	public int maxDiff(int[] arr) {
-		
-		if(arr==null || arr.length<2) return -1;
-		
-		int min = arr[0];
-		int maxDiff = arr[1]-min;
-		for (int i = 1; i < arr.length; i++) {
-			if(arr[i-1]<min) {
-				min = arr[i-1];
-			}
-			if(arr[i]-min>maxDiff) {
-				maxDiff = arr[i]-min;
-			}
-		}
-		
-		return maxDiff;
-		
-	}
-	
-	
+    public int maxDiff(int[] arr) {
+        if (arr == null || arr.length < 2) return -1;
+
+        int min = arr[0];
+        int maxDiff = arr[1] - min;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] < min) {
+                min = arr[i - 1];
+            }
+            if (arr[i] - min > maxDiff) {
+                maxDiff = arr[i] - min;
+            }
+        }
+
+        return maxDiff;
+    }
 }
--- src/cn/itwx7/Offer65.java
+++ src/cn/itwx7/Offer65.java
@@ -2,20 +2,18 @@ package cn.itwx7;
 
 /**
  * 不用加减乘除作加法
- * @author wang
  *
+ * @author wang
  */
 public class Offer65 {
 
-	public int add(int num1,int num2) {
-		while(num2!=0) {
-			int sum = num1^num2;
-			int carry = (num1&num2)<<1;
-			num1 = sum;
-			num2 = carry;
-		}
-		return num1;
-	}
-	
-	
+    public int add(int num1, int num2) {
+        while (num2 != 0) {
+            int sum = num1 ^ num2;
+            int carry = (num1 & num2) << 1;
+            num1 = sum;
+            num2 = carry;
+        }
+        return num1;
+    }
 }
